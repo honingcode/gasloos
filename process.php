202180a8190ab95b023abcafea5537cba764eee6
honingcode <levihoning@gmail.com>
2023-02-22 17:00:40 +0100

    Update process.php

--- process.php
+++ process.php
@@ -1,33 +1,32 @@
 
 <?php
-    print('hello');
-    // // Get form data
-    // $name = $_POST['name'];
-    // $email = $_POST['email'];
-    // $message = $_POST['message'];
+    // Get form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-    // // Validate form data
-    // if (empty($name) || empty($email) || empty($message)) {
-    //     die('Please fill in all fields.');
-    // }
-    // if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    //     die('Invalid email address.');
-    // }
+    // Validate form data
+    if (empty($name) || empty($email) || empty($message)) {
+        die('Please fill in all fields.');
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        die('Invalid email address.');
+    }
 
-    // // Sanitize form data
-    // $name = filter_var($name, FILTER_SANITIZE_STRING);
-    // $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-    // $message = filter_var($message, FILTER_SANITIZE_STRING);
+    // Sanitize form data
+    $name = filter_var($name, FILTER_SANITIZE_STRING);
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $message = filter_var($message, FILTER_SANITIZE_STRING);
 
-    // // Send email
-    // $to = 'levihoning@gmail.com';
-    // $subject = 'New message from your website';
-    // $body = "Name: $name\nEmail: $email\nMessage:\n$message";
-    // $headers = "From: $email\r\n";
-    // $headers .= "Reply-To: $email\r\n";
-    // $headers .= "X-Mailer: PHP/" . phpversion();
-    // mail($to, $subject, $body, $headers);
+    // Send email
+    $to = 'levihoning@gmail.com';
+    $subject = 'New message from your website';
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
+    mail($to, $subject, $body, $headers);
 
-    // // Redirect to thank you page
-    // header('Location: index.html');
+    // Redirect to thank you page
+    header('Location: index.html');
 ?>
